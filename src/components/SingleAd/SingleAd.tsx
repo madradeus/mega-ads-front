9f3ae3ba742fa8a8e12ac5d200134df2fc0508f9
Mateusz Rodowski <mateusz.rodowski@gmail.com>
2023-02-15 22:15:42 +0100

    Added style refactor

--- src/components/SingleAd/SingleAd.tsx
+++ src/components/SingleAd/SingleAd.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { AdEntity } from 'types';
 import { api } from "../../lib/api";
 import { Loader } from "../common/Loader/Loader";
+import { useToast } from "@chakra-ui/react";
 
 interface Props {
     id: string;
@@ -10,17 +11,30 @@ interface Props {
 export const SingleAd = ({ id }: Props) => {
 
     const [ad, setAd] = useState<null | AdEntity>(null);
+    const toast = useToast()
 
     useEffect(() => {
         (async () => {
-            const adData = await api.getOne(id);
-            setAd(adData)
+            try {
+                const adData = await api.getOne(id);
+                setAd(adData);
+            } catch (e: any) {
+                toast({
+                    position: "top-right",
+                    title: 'Error',
+                    description: e.message,
+                    status: 'error',
+                    duration: 3000,
+                })
+            }
         })()
     }, []);
 
     if ( !ad ) {
         return <Loader/>
+
     }
+
     return (
         <div className="ad-info">
 
